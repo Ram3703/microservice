3440e253c1c16b031a8975dffe45274885fae7ad
Ram3703 <78849459+Ram3703@users.noreply.github.com>
2021-03-24 16:19:19 +0530

    Add files via upload

---
+++ MicroServers/src/test/java/com/ltts/MicroServers/MicroServersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltts.MicroServers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
